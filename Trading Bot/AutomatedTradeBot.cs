a6b8f2eeb01c85a41a84156131dba611207721be
Sylas Coker <60179137+Syycoker@users.noreply.github.com>
2022-02-11 20:00:43 +0000

    Created 'BuySystem' static class to handle buy orders, facing concurrency issues, on route to fixing issues

--- Trading Bot/AutomatedTradeBot.cs
+++ Trading Bot/AutomatedTradeBot.cs
@@ -34,12 +34,6 @@ namespace Trading_Bot
         // Initialise the authorisation codes.
         AuthenticationConfig.Initialise();
 
-        // Initialise the database.
-        //DatabaseConfig.Initialise();
-
-        // Initialise the socket [DEPRECTAED]
-        // SocketConfig.Initialise();
-
         Console.ForegroundColor = ConsoleColor.White;
 
         HttpClient hClient = new HttpClient();
@@ -50,7 +44,6 @@ namespace Trading_Bot
         Task.Run(async () =>
         {
           await UpdateAvailableCoins();
-          await GetAssetDetail(AvailableCoins[0]);
         }).GetAwaiter().GetResult();
       }
 
@@ -65,35 +58,6 @@ namespace Trading_Bot
     }
 
     #region Main Procedures
-    /// <summary>
-    /// Stores the trade pairs (key).
-    /// </summary>
-    public static List<string> AvailableCoins = new();
-
-    private bool StartRoutines(int buyThreadCount = 1, int sellThreadCount = 1)
-    {
-      try
-      {
-        // Create the threads.
-        for (int i = 0; i < buyThreadCount; i++)
-        {
-          // Thread buyThread = new Thread() { IsBackground = true };
-          // buyThread.Start();
-        }
-
-        for (int j = 0; j < sellThreadCount; j++)
-        {
-          // Thread sellThread = new Thread() { IsBackground = true };
-          // sellThread.Start();
-        }
-        return true;
-      }
-      catch
-      {
-        // Swallow Exceptions
-        return false;
-      }
-    }
 
     /// <summary>
     /// Gets all the available and tradeable coins on the market by returning the product's name, i.e. "BTC"...
@@ -105,8 +69,6 @@ namespace Trading_Bot
       {
         await Semaphore.WaitAsync();
 
-        // Clear any coins saved already and start afresh.
-        AvailableCoins.Clear();
         string response = await BClient.SendSignedAsync("/sapi/v1/capital/config/getall", HttpMethod.Get);
         var products = JArray.Parse(response);
 
@@ -115,7 +77,7 @@ namespace Trading_Bot
           // Not interested in products that are not tradeable.
           if (!product["trading"].Value<bool>() == true) { continue; }
 
-          AvailableCoins.Add(product["coin"].Value<string>());
+          // AvailableCoins.Add(product["coin"].Value<string>());
         }
       }
       catch
@@ -123,80 +85,6 @@ namespace Trading_Bot
         // Swallow Exception for now, will create logging system
         // or maybe create frontend for mobile app that sends notifications to my phone, who knows. for now
         //do everything backened.
-      }
-      finally
-      {
-        Semaphore.Release();
-      }
-    }
-
-    /// <summary>
-    /// Returns the details about aspecific asset.
-    /// </summary>
-    /// <returns>A response string.</returns>
-    private static async Task<string> GetAssetDetail(string assetName)
-    {
-      Dictionary<string, object> assetDictionary = new();
-      assetDictionary.Add("asset", assetName);
-      string response = await BClient.SendSignedAsync("/sapi/v1/asset/assetDetail", HttpMethod.Get, assetDictionary);
-      return response;
-    }
-
-    /// <summary>
-    /// Attempts to buy a single product by placing a bid slightly below it's average sell price in the marketplace.
-    /// </summary>
-    /// <param name="synbol"></param>
-    /// <param name="bidAmount"></param>
-    /// <returns></returns>
-    private static async Task<bool> BuyAsset(string symbol, decimal bidAmount)
-    {
-      try
-      {
-        Dictionary<string, object> parameters = new();
-
-        // Setting the parameters for this request as i'll only ever want to buy one asset at a time.
-        parameters.Add("symbol", symbol);
-        parameters.Add("side", "BUY");
-        parameters.Add("type", "LIMIT");
-        parameters.Add("quantity", 1);
-        parameters.Add("price", bidAmount);
-        string response = await BClient.SendSignedAsync("/api/v3/order", HttpMethod.Post, parameters);
-        return true;
-      }
-      catch
-      {
-        // Swallow Exception
-        return false;
-      }
-    }
-
-    private void Sell()
-    {
-
-    }
-
-    /// <summary>
-    /// Returns an enumarable collection of coins that can be bought / traded for at *this* very moment.
-    /// </summary>
-    private static async Task<List<PTrade>> GetExchangeInfo()
-    {
-      try
-      {
-        // Get all the coins in the exchange
-        await Semaphore.WaitAsync();
-        Dictionary<string, decimal> exchange = new();
-
-        var response = await BClient.SendSignedAsync("/api/v1/exchangeInfo", HttpMethod.Get);
-        
-        Console.WriteLine(response);
-
-        // Sort each pTrade by their analysis eval
-        return null;
-      }
-      catch (Exception e)
-      {
-        Console.WriteLine(e.Message);
-        throw new Exception("Unable to find available coins from Client call.");
       }
       finally
       {
---
+++ Trading Bot/BuySystem.cs
@@ -0,0 +1,169 @@
+﻿using BinanceDotNet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Trading_Bot
+{
+  public static class BuySystem
+  {
+    #region Client
+    public static BinanceService BClient { get; set; }
+    #endregion
+    #region Thread Safety
+    private static SemaphoreSlim Semaphore = new SemaphoreSlim(1, 1);
+    #endregion
+    public static void AnalyseMarket()
+    {
+      try
+      {
+        while (true)
+        {
+          // First step, look for every coin in the marketplace
+          List<string> availableCoins = GetExchangeInfo().Result.ToList();
+        }
+      }
+      catch(Exception e)
+      {
+        // If an exception has occured here, send a notification!!
+        Console.WriteLine("Error has occured when analysing marketplace.");
+        Console.WriteLine(e.Message);
+      }
+    }
+
+    /// <summary>
+    /// Returns a generalised trade fee.
+    /// </summary>
+    public static decimal Fee { get; set; } = GetTradeFee().Result;
+
+    /// <summary>
+    /// Gets the trade fee of a pair.
+    /// </summary>
+    /// <returns>Returns a decimal value of the trade fee. </returns>
+    public static async Task<decimal> GetTradeFee(string pairSymbol = "")
+    {
+      try
+      {
+        await Semaphore.WaitAsync();
+
+        Console.WriteLine("Getting Trade Fee...");
+
+        Dictionary<string, object> param = new();
+        param.Add("symbol", pairSymbol);
+
+        string response = string.Empty;
+
+        if (string.IsNullOrEmpty(pairSymbol)) { response = await BClient.SendSignedAsync("sapi/v1/asset/tradeFee", HttpMethod.Get); }
+        else { response = await BClient.SendSignedAsync("sapi/v1/asset/tradeFee", HttpMethod.Get, param); }
+
+        decimal responseVal = decimal.Parse(response);
+        Console.WriteLine("Trade fee set.");
+        return responseVal;
+      }
+      catch
+      {
+        // Swallow and send an invalid response.
+        return decimal.MinValue;
+      }
+      finally
+      {
+        Semaphore.Release();
+      }
+    }
+
+    /// <summary>
+    /// Attempts to buy a single product by placing a bid slightly below it's average sell price in the marketplace.
+    /// </summary>
+    /// <param name="synbol"></param>
+    /// <param name="bidAmount"></param>
+    /// <returns>True if the asset has been 'bought'.</returns>
+    public static async Task<bool> BuyAsset(string symbol, decimal bidAmount)
+    {
+      try
+      {
+        await Semaphore.WaitAsync();
+
+        Dictionary<string, object> parameters = new();
+
+        // Setting the parameters for this request as i'll only ever want to buy one asset at a time.
+        parameters.Add("symbol", symbol);
+        parameters.Add("side", "BUY");
+        parameters.Add("type", "LIMIT");
+        parameters.Add("quantity", 1);
+        parameters.Add("price", bidAmount);
+        string response = await BClient.SendSignedAsync("/api/v3/order", HttpMethod.Post, parameters);
+        return true;
+      }
+      catch
+      {
+        // Swallow Exception
+        return false;
+      }
+      finally
+      {
+        Semaphore.Release();
+      }
+    }
+
+    /// <summary>
+    /// Returns a tuple of the asset name and amount held buy the asset
+    /// </summary>
+    /// <returns></returns>
+    public static async Task<List<(string, decimal)>> GetWallet()
+    {
+      try
+      {
+        await Semaphore.WaitAsync();
+
+        List<(string, decimal)> result = new();
+
+        string response = await BClient.SendSignedAsync("/api/v3/account", HttpMethod.Get);
+
+        return result;
+      }
+      catch
+      {
+        return null;
+      }
+      finally
+      {
+        Semaphore.Release();
+      }
+    }
+
+    /// <summary>
+    /// Returns an enumarable collection of coins that can be bought / traded for at *this* very moment.
+    /// </summary>
+    private static async Task<IEnumerable<string>> GetExchangeInfo()
+    {
+      try
+      {
+        // Attempt to get all the coins in the exchange
+        await Semaphore.WaitAsync();
+
+        Dictionary<string, decimal> exchange = new();
+
+        var response = await BClient.SendSignedAsync("/api/v1/exchangeInfo", HttpMethod.Get);
+
+        Console.WriteLine(response);
+
+        // Sort each pTrade by their analysis eval
+        return null;
+      }
+      catch (Exception e)
+      {
+        Console.WriteLine(e.Message);
+        throw new Exception("Unable to find available coins from Client call.");
+      }
+      finally
+      {
+        Semaphore.Release();
+      }
+    }
+    // Write a 'rollback' system for when a buy order has been unsuccessful.
+  }
+}
